71b3e63b822d4bdb377e9ce5e6fff82a4806b6b4
kaden-emrich <125313480+kaden-emrich@users.noreply.github.com>
2023-10-20 11:27:45 -0400

    tic tac toe finished started on bot

--- play/game.js
+++ play/game.js
@@ -1,4 +1,6 @@
 var game = document.getElementById("game");
+var gameStateDisplay = document.getElementById("game-state-display");
+
 const NUM_columnS = 3;
 const NUM_ROWS = 3;
 const NUM_CELLS = NUM_columnS + NUM_ROWS;
@@ -20,7 +22,7 @@ const WIN_COMBINATIONS = [
 class TicTacToe {
     constructor(parentElement) {
         this.parentElement = parentElement;
-        this.cells = [[]];
+        this.cells = [];
 
         // create board
         this.board = document.createElement('div');
@@ -30,7 +32,6 @@ class TicTacToe {
 
         for(let row = 0; row < NUM_ROWS; row++) {
             let rowClass = ROW_CLASS_LIST[row];
-            this.cells[row] = [];
             
             for(let column = 0; column < NUM_columnS; column++) {
                 let columnClass = column_CLASS_LIST[column];
@@ -39,14 +40,15 @@ class TicTacToe {
                 nextCell.setAttribute('class', `cell ${rowClass} ${columnClass}`);
                 nextCell.setAttribute('data-row', row);
                 nextCell.setAttribute('data-column', column);
+                let cellIndex = this.cells.length;
             
                 nextCell.addEventListener('click', () => {
-                    this.cellClick(column, row);
+                    this.cellClick(cellIndex);
                 });
 
                 this.board.appendChild(nextCell);
 
-                this.cells[row][column] = nextCell;
+                this.cells[this.cells.length] = nextCell;
             }
             
             this.board.appendChild(document.createElement('br'));
@@ -55,21 +57,30 @@ class TicTacToe {
         this.parentElement.appendChild(this.board);
     }// constructor(parentElement)
 
-    checkWin(playerDocClass) { //https://stackoverflow.com/questions/1056316/algorithm-for-determining-tic-tac-toe-game-over#:~:text=To%20check%20whether%20a%20player,if%20the%20two%20are%20equal.&text=eg.&text=Note%3A%20X%20%26%20W%20%3D%20W,is%20in%20a%20win%20state.
-        var positions = 0;
-        var index = 0;
+    getCurrentPlayerDocClass() {
+        if(this.playerTurn == 1) {
+            return 'marked-x';
+        }
+        else if(this.playerTurn == 2) {
+            return 'marked-o';
+        }
+        
+        return '';
+    }
 
-        for(let row = 0; row < this.cells.length; row++) {
-            for(let column = 0; column < this.cells[row].length; column++) {
-                if(this.cells[row][column].classList.contains(playerDocClass)) {
-                    positions += Math.pow(2, index);
-                }
-                index++;
+    getBinary(playerDocClass) {
+        var positions = 0;
+        for(let cellIndex in this.cells) {
+            if(this.cells[cellIndex].classList.contains(playerDocClass)) {
+                positions += Math.pow(2, cellIndex);
             }
         }
+        return positions;
+    }
 
-        for(let win in WIN_COMBINATIONS) {
-            if(positions & win == win) {
+    static checkBinaryWin(binary) {
+        for(let win of WIN_COMBINATIONS) {
+            if((binary & win) == win) {
                 return true;
             }
         }
@@ -77,27 +88,131 @@ class TicTacToe {
         return false;
     }
 
-    checkGameOver() {
-
+    checkWin(playerDocClass) {
+        return TicTacToe.checkBinaryWin(this.getBinary(playerDocClass));
     }
 
-    cellClick(x, y) {
-        if(this.cells[y][x].classList.contains('marked-x') || this.cells[y][x].classList.contains('marked-o')) {
+    cellIsOpen(id) {
+        if(!(this.cells[id].classList.contains('marked-x') || this.cells[id].classList.contains('marked-o'))) {
+            return true;
+        }
+        return false;
+    }
+
+    checkOpenCells() {
+        for(let cellIndex in this.cells) {
+            if(this.cellIsOpen(cellIndex)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    endGame() {
+        this.gameOver = true;
+        this.playerTurn = 0;
+        this.parentElement.firstElementChild.classList.add('game-over');
+    }
+
+    checkGameOver() {
+        if(this.checkWin('marked-x')) {
+            this.parentElement.classList.add('marked-x');
+            this.endGame();
+        }
+        else if(this.checkWin('marked-o')) {
+            this.parentElement.classList.add('marked-o');
+            this.endGame();
+        }
+        else if(!this.checkOpenCells()) {
+            this.parentElement.classList.add('tie');
+            this.endGame();
+        }
+    }
+
+    nextTurn() {
+        this.checkGameOver();
+
+        if(this.playerTurn == 1) {
+            this.playerTurn = 2;
+        }
+        else if(this.playerTurn == 2){
+            this.playerTurn = 1;
+        }
+        else {
+            this.playerTurn = 0;
+        }
+    }
+
+    cellClick(cellIndex) {
+        if(this.playerTurn == 0) {
+            return;
+        }
+
+        if(this.cells[cellIndex].classList.contains('marked-x') || this.cells[cellIndex].classList.contains('marked-o')) {
             return;
         }
 
         if(this.playerTurn == 1) {
-            this.cells[y][x].classList.add('marked-x');
-            this.playerTurn = 2;
+            this.cells[cellIndex].classList.add('marked-x');
         }
         else if(this.playerTurn == 2){
-            this.cells[y][x].classList.add('marked-o');
-            this.playerTurn = 1;
+            this.cells[cellIndex].classList.add('marked-o');
         }
-        
 
+        this.nextTurn();
     }// cellClick(game, x, y)
 
 }// class TicTacToe
 
+class TicTacBot {
+    constructor(game) {
+        this.game = game;
+    }// constructor(parentElement)
+
+    checkWinMoves(playerDocClass) {
+        for(let cellIndex in this.game.cells) {
+            if(this.game.cellIsOpen(cellIndex)) {
+                let cellBinary = this.game.getBinary(playerDocClass) + Math.pow(2, cellIndex);
+                if(TicTacToe.checkBinaryWin(cellBinary)) {
+                    return parseInt(cellIndex);
+                }
+            }
+        }
+        return -1;
+    }
+
+    getBestMove() {
+        let botTurn = 'marked-o';
+        let otherTurn = 'marked-x';
+
+        // is game active
+        if(this.game.playerTurn == 1) {
+            botTurn = 'marked-x';
+            otherTurn = 'marked-o';
+        }
+        else if(this.game.playerTurn == 2) {
+            botTurn = 'marked-o';
+            otherTurn = 'marked-x';
+        }
+
+        // check for wins
+        if(this.checkWinMoves(botTurn)) {
+
+        }
+
+        // check for blocks
+        if(this.checkWinMoves(otherTurn)) {
+            return this.checkWinMoves(otherTurn);
+        }
+
+        // 
+        
+
+        
+    }
+    
+}// class TicTacBot
+
 var gameBoard = new TicTacToe(game);
+var gameBot = new TicTacBot(gameBoard);
