945cf493c74efeb4a7f13151700da3af9da4cafc
Kaden Emrich <kadenemrich@gmail.com>
2024-04-16 14:13:02 -0400

    min-max-alg attempt

--- play/game.js
+++ play/game.js
@@ -31,6 +31,10 @@ const BOT_MOVE_PRIORITY = [
     7
 ];
 
+function dec2bin(dec) {
+    return (dec >>> 0).toString(2);
+}
+
 class TicTacToe {
     constructor(parentElement) {
         this.parentElement = parentElement;
@@ -160,6 +164,7 @@ class TicTacToe {
         if(this.bot && this.bot.player && this.bot.player == this.playerTurn) {
             this.cellClick(this.bot.getBestMove());
         }
+        sBot.getBestMove();
     }
 
     cellClick(cellIndex) {
@@ -183,8 +188,41 @@ class TicTacToe {
             //console.log('Player 2 moved to ' + cellIndex);
         }
 
+        for(let cell of this.cells) {
+            cell.classList.remove('highlighted');
+            cell.innerText = '';
+        }
+
         this.nextTurn();
     }// cellClick(game, x, y)
+
+    transposeValues(arr) {
+        for(let i = 0; i < this.cells.length; i++) {
+            this.cells[i].classList.remove('highlighted');
+        }
+
+        let max = 0;
+        for(let i = 0; i < arr.length; i++) {
+            if(arr[max] == undefined) {
+                max = i;
+            }
+            if(arr[i] && arr[max] && arr[i] > arr[max]) {
+                max = i;
+            }
+        }
+        
+        for(let i = 0; i < arr.length; i++) {
+            if(arr[i] == undefined) {
+                this.cells[i].innerText = '';
+            }
+            else {
+                this.cells[i].innerText = arr[i];
+            }
+            if(i == max) {
+                this.cells[i].classList.add('highlighted');
+            }
+        }
+    }
 
     reset() {
         this.parentElement.classList.remove('marked-x');
@@ -200,6 +238,8 @@ class TicTacToe {
         for(let cell of this.cells) {
             cell.classList.remove('marked-x');
             cell.classList.remove('marked-o');
+            cell.classList.remove('highlighted');
+            cell.innerText = '';
         }
     }
 
@@ -338,7 +378,87 @@ class TicTacBot {
     }
 }// class TicTacBot
 
+class superBot {
+    constructor(game, foresight = 18) {
+        this.game = game;
+        this.foresight = foresight;
+    }// constructor(parentElement)
+
+    static testPosibleMove(takenSpots, move) {
+        if((takenSpots & move) == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    simNextTurn(botMove, otherMove, botTurn = true, lookAhead) {
+        if(lookAhead <= 0) {
+            // console.log('foresight abandon');
+            return 0;
+        }
+        if((botMove & otherMove) >= 31) {
+            return lookAhead;
+        }
+        if(TicTacToe.checkBinaryWin(otherMove)) {
+            return 0-(lookAhead);
+        }
+        if(TicTacToe.checkBinaryWin(botMove)) {
+            return lookAhead;
+        }
+
+        // console.log(`starting turn on layer ${lookAhead} with ${(botMove >>> 0).toString(2)} and ${(otherMove >>> 0).toString(2)} (${botTurn ? 'bot' : 'not bot'})`); // for debugging
+
+        let moveValue = 0;
+        for(let i = 0; i < 9; i++) {
+            let thisMove = 2 ** i;
+            if(superBot.testPosibleMove(botMove | otherMove, thisMove)) {
+                if(botTurn) {
+                    moveValue += this.simNextTurn(botMove + thisMove, otherMove, !botTurn, lookAhead-1);
+                }
+                else {
+                    moveValue += this.simNextTurn(botMove, otherMove + thisMove, !botTurn, lookAhead-1);
+                }
+            }
+        }
+        return moveValue;
+    }
+
+    getWeightedMoveValues(botMoves, otherMoves, lookAhead = 18) {
+        let weightedValues = Array(9);
+        for(let i = 0; i < 9; i++) {
+            let thisMove = 2 ** i;
+            if(superBot.testPosibleMove(botMoves | otherMoves, thisMove)) {
+                weightedValues[i] = this.simNextTurn(botMoves + thisMove, otherMoves, true, lookAhead);
+            }
+        }
+        return weightedValues;
+    }
+
+    getBestMove() {
+        let botTurn = 'marked-o';
+        let otherTurn = 'marked-x';
+
+        // is game active
+        if(this.game.playerTurn == 1) {
+            botTurn = 'marked-x';
+            otherTurn = 'marked-o';
+        }
+        else if(this.game.playerTurn == 2) {
+            botTurn = 'marked-o';
+            otherTurn = 'marked-x';
+        }
+
+        let botMoves = this.game.getBinary(botTurn);
+        let otherMoves = this.game.getBinary(otherTurn);
+
+        let values = this.getWeightedMoveValues(botMoves, otherMoves, this.foresight);
+        this.game.transposeValues(values);
+        return values;
+    }
+}// class superBot
+
 var gameBoard = new TicTacToe(game);
+var sBot = new superBot(gameBoard, 18);
 
 function newGame(botPlayer) {
     console.log('new game');
@@ -349,4 +469,6 @@ function newGame(botPlayer) {
     else if(botPlayer == 2) {
         gameBoard.bot = new TicTacBot(gameBoard, 2);
     }
+
+    sBot = new superBot(gameBoard, 18); 
 }
